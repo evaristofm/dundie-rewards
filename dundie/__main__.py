d7790e14bc3407253a1c0c32ae921689f7a8835f
Evaristo Ferreira <evaristo.ferreira@propig.com.br>
2022-05-29 23:29:37 -0300

    add entry point

---
+++ dundie/__main__.py
@@ -0,0 +1,3 @@
+
+def main():
+    print("Execute dundie entry point")
--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+from importlib.metadata import entry_points
 from setuptools import find_packages, setup
 
 
 setup(
+
     name="dundie",
     version="0.1.0",
-    description="Reward Point System for Dunder Mifflin"
+    description="Reward Point System for Dunder Mifflin",
     author="Evaristo Neto",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    }
 )
