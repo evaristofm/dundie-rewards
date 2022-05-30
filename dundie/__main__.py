68fb427383703e11624f321c7df3888b2a923775
Evaristo Ferreira <evaristo.ferreira@propig.com.br>
2022-05-30 16:33:39 -0300

    add tests

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,39 +1,4 @@
-import argparse
-
-
-
-def load(filepath):
-    """loads data from filepath to the database"""
-    try:
-        with open(filepath) as file_:
-            for line in file_:
-                print(line)
-    except FileNotFoundError as e:
-        print(f"File not found {e}")
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description="Dunder Mifflin Rewards CLI",
-        epilog="Enjoy and use with cautious.",
-    )
-    parser.add_argument(
-        "subcommand",
-        type=str,
-        help="The sub command to run",
-        choices=("load", "show", "send"),
-    )
-    parser.add_argument(
-        "filepath",
-        type=str,
-        help="File path to load",
-    )
-    args = parser.parse_args()
-    
-    try:
-        globals()[args.subcommand](args.filepath)
-    except KeyError:
-        print("Subcommand is invalid.")
+from dundie.cli import main
 
 
 
