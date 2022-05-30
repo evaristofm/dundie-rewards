43e0fcdee3026bdb76e52654a01627706c3b9a7a
Evaristo Ferreira <evaristo.ferreira@propig.com.br>
2022-05-30 00:54:54 -0300

    add makefile

---
+++ Makefile
@@ -0,0 +1,3 @@
+install:
+	@echo "Installing for dev enviroment"
+	@.venv/Scripts/python -m pip install -e .[dev]
--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,3 +1,41 @@
+import argparse
+
+
+
+def load(filepath):
+    """loads data from filepath to the database"""
+    try:
+        with open(filepath) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError as e:
+        print(f"File not found {e}")
+
 
 def main():
-    print("Execute dundie entry point")
+    parser = argparse.ArgumentParser(
+        description="Dunder Mifflin Rewards CLI",
+        epilog="Enjoy and use with cautious.",
+    )
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="The sub command to run",
+        choices=("load", "show", "send"),
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="File path to load",
+    )
+    args = parser.parse_args()
+    
+    try:
+        globals()[args.subcommand](args.filepath)
+    except KeyError:
+        print("Subcommand is invalid.")
+
+
+
+if __name__ == "__main__":
+    main()
