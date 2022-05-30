31b8a67ec2fb69305bd2206727675c6c859cbf86
Evaristo Ferreira <evaristo.ferreira@propig.com.br>
2022-05-29 22:30:17 -0300

    Set up project initial organization and metadata

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin"
+    author="Evaristo Neto",
+    packages=find_packages(),
+)
