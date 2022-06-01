66db01972e4696596ae42f7a5d09471f273ae943
Evaristo Ferreira <evaristo.ferreira@propig.com.br>
2022-06-01 20:44:02 -0300

    more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import pytest
 MARKER = """
 integration: Mark integration tests
 high: High Priority
@@ -9,3 +10,10 @@ low: Low Priority
 def pytest_configure(config):
     for line in MARKER.split("\n"):
         config.addinivalue_line('markers', line)
+
+
+@pytest.fixture(autouse=True)
+def go_to_tmp_dir(request):
+    tmpdir = request.getfixturevalue("tmpdir")
+    with tmpdir.as_cwd():
+        yield   # protocolo de generators
